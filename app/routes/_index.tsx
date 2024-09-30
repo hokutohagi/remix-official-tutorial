962e47e1ec6c4c29761dd81434eaa2704613573d
HAGIWARA Hokuto <hokuto.hagiwara@circu.co.jp>
2024-09-30 23:29:20 +0900

    インデックスルート実装

---
+++ app/routes/_index.tsx
@@ -0,0 +1,10 @@
+export default function Index() {
+    return (
+      <p id="index-page">
+        これは Remix のデモです。
+        <br />
+        <a href="https://remix.run">remix.run のドキュメント</a> を見てください。
+      </p>
+    );
+  }
+  
