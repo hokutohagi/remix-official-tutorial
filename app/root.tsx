48e3e1fb3c0285f6f3c9ce5bdf73490896048b21
HAGIWARA Hokuto <hokuto.hagiwara@circu.co.jp>
2024-09-30 22:13:43 +0900

    active なリンクを表示

--- app/root.tsx
+++ app/root.tsx
@@ -3,9 +3,9 @@ import { json, redirect } from "@remix-run/node";
 
 import {
   Form,
-  Link,
   Links,
   Meta,
+  NavLink,
   Outlet,
   Scripts,
   ScrollRestoration,
@@ -65,7 +65,16 @@ export default function App() {
               <ul>
                 {contacts.map((contact) => (
                   <li key={contact.id}>
-                    <Link to={`contacts/${contact.id}`}>
+                    <NavLink
+                      className={({ isActive, isPending }) =>
+                        isActive
+                          ? "active"
+                          : isPending
+                          ? "pending"
+                          : ""
+                      }
+                      to={`contacts/${contact.id}`}
+                    >
                       {contact.first || contact.last ? (
                         <>
                           {contact.first} {contact.last}
@@ -76,7 +85,7 @@ export default function App() {
                       {contact.favorite ? (
                         <span>★</span>
                       ) : null}
-                    </Link>
+                    </NavLink>
                   </li>
                 ))}
               </ul>
