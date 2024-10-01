f1af947914f6582bccc2ca61f7063211a3f7dc7f
HAGIWARA Hokuto <hokuto.hagiwara@circu.co.jp>
2024-10-01 21:50:14 +0900

    戻るボタン押下時に維持

--- app/root.tsx
+++ app/root.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import type { LinksFunction, LoaderFunctionArgs } from "@remix-run/node";
 import { json, redirect } from "@remix-run/node";
 
@@ -33,12 +34,19 @@ export const loader = async ({
   const url = new URL(request.url);
   const q = url.searchParams.get("q");
   const contacts = await getContacts(q);
-  return json({ contacts });
+  return json({ contacts, q });
 };
 
 export default function App() {
-  const { contacts } = useLoaderData<typeof loader>();
+  const { contacts, q } = useLoaderData<typeof loader>();
   const navigation = useNavigation();
+
+  useEffect(() => {
+    const searchField = document.getElementById("q");
+    if (searchField instanceof HTMLInputElement) {
+      searchField.value = q || "";
+    }
+  }, [q]);
 
   return (
     <html lang="en">
@@ -55,10 +63,11 @@ export default function App() {
             <Form id="search-form" role="search">
               <input
                 id="q"
+                name="q"
+                defaultValue={q || ""}
                 aria-label="Search contacts"
                 placeholder="Search"
                 type="search"
-                name="q"
               />
               <div id="search-spinner" aria-hidden hidden={true} />
             </Form>
